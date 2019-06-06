9a5c82513a6cab3aa343c11118913a2b91aa9a8f
IronMan <ironmanandrej@gmail.com>
2019-06-06 21:35:07 +0300

    third

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const request = new XMLHttpRequest
-
+request.open("GET","хрень.json")	
 request.onreadystatechange  =  function(){
 	if (this.readyState === 4 )
 		if (this.status === 200 )
-			this.console.log(JSON.parse(this.responseText))
+			getcontent2(JSON.parse(this.responseText))
 }
-request.open("GET""хрень.json")
+
 request.send()	
+
+
+function getcontent2(tags2){
+                  var ex2 = document.getElementById("example2")
+                  for (var stag of tags2){
+	                   	var newelem = document.createElement(stag.name)
+	                   	ex2.appendChild(newelem)
+	                  	newelem.classList.add(stag.class)
+	                  	style.appendChild(document.createTextNode(`.${stag.class} { ${stag.style}}`))
+	                  	newelem.innerText =` Кликни на этот ${stag.name}`
+                      }
+                    }
+                    
+                    function evenfunc(event){
+                    alert("Клик")
+                    }
